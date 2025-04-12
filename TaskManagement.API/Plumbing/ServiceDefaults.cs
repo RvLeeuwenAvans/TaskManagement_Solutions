3f14c6512aa5b5bb2fc56944d7754b1202a6e9f0
RvLeeuwenAvans <r.vanleeuwen2@student.avans.nl>
2025-04-12 14:38:41 +0200

    18 - Initialize API skeleton
    * add git ignore
    
    * add git ignores
    
    * Setup skeleton to kickoff development; created a class library to store API-models.

---
+++ TaskManagement.API/Plumbing/ServiceDefaults.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagement.API.Plumbing;
+
+public static class ServiceDefaults
+{
+    public static IServiceCollection ConfigureDefaultServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
