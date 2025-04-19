a4ff3862485c1e22cc41ac3ecaebbc9c99519e6f
Ridwan <r.vanleeuwen2@student.avans.nl>
2025-04-19 16:07:56 +0200

    Create service, validation and repo logic for userTask

---
+++ TaskManagement.Application/Services/UserTaskService.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using TaskManagement.Application.Interfaces.Repositories;
+using TaskManagement.Domain.Office.User.Task;
+using TaskManagement.DTO.Office.User.Task;
+
+namespace TaskManagement.Application.Services;
+
+public class UserTaskService(
+    IUserTaskRepository taskRepository,
+    IMapper mapper,
+    IValidator<UserTaskCreateDto> createValidator,
+    IValidator<UserTaskUpdateDto> updateValidator)
+{
+    public async Task<List<UserTaskResponseDto>> GetAllTasksAsync()
+    {
+        var tasks = await taskRepository.GetAll().ToListAsync();
+        return mapper.Map<List<UserTaskResponseDto>>(tasks);
+    }
+
+    public async Task<UserTaskResponseDto?> GetTaskByIdAsync(Guid id)
+    {
+        var task = await taskRepository.GetByIdAsync(id);
+        return task is null ? null : mapper.Map<UserTaskResponseDto>(task);
+    }
+
+    public async Task<UserTaskResponseDto> CreateTaskAsync(UserTaskCreateDto dto)
+    {
+        var validationResult = await createValidator.ValidateAsync(dto);
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var task = mapper.Map<UserTask>(dto);
+        await taskRepository.AddAsync(task);
+        return mapper.Map<UserTaskResponseDto>(task);
+    }
+
+    public async Task<bool> UpdateTaskAsync(UserTaskUpdateDto dto)
+    {
+        var validationResult = await updateValidator.ValidateAsync(dto);
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var task = await taskRepository.GetByIdAsync(dto.Id);
+        if (task is null)
+            return false;
+
+        mapper.Map(dto, task);
+        await taskRepository.UpdateAsync(task);
+        return true;
+    }
+
+    public async Task<bool> DeleteTaskAsync(Guid id)
+    {
+        var task = await taskRepository.GetByIdAsync(id);
+        if (task is null)
+            return false;
+
+        await taskRepository.DeleteAsync(id);
+        return true;
+    }
+}
---
+++ TaskManagement.DTO/Office/User/Task/Validators/UserTaskUpdateDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace TaskManagement.DTO.Office.User.Task.Validators;
+
+public class UserTaskUpdateDtoValidator : AbstractValidator<UserTaskUpdateDto>
+{
+    public UserTaskUpdateDtoValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty();
+
+        When(x => x.Title is not null, () =>
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .MaximumLength(50);
+        });
+
+        When(x => x.Description is not null, () =>
+        {
+            RuleFor(x => x.Description)
+                .MaximumLength(255);
+        });
+    }
+}
