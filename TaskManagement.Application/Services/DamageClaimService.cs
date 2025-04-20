57ef8592d846cc5b41fd8ab9477d3ed2784af3a6
Ridwan <r.vanleeuwen2@student.avans.nl>
2025-04-20 18:34:27 +0200

    formatting

--- TaskManagement.Application/Services/DamageClaimService.cs
+++ TaskManagement.Application/Services/DamageClaimService.cs
@@ -4,60 +4,59 @@ using TaskManagement.Application.Interfaces.Repositories;
 using TaskManagement.Domain.Office.Relation.DamageClaim;
 using TaskManagement.DTO.Office.Relation.DamageClaim;
 
-namespace TaskManagement.Application.Services
+namespace TaskManagement.Application.Services;
+
+public class DamageClaimService(
+    IDamageClaimRepository damageClaimRepository,
+    IMapper mapper,
+    IValidator<DamageClaimCreateDto> createValidator,
+    IValidator<DamageClaimUpdateDto> updateValidator)
 {
-    public class DamageClaimService(
-        IDamageClaimRepository damageClaimRepository,
-        IMapper mapper,
-        IValidator<DamageClaimCreateDto> createValidator,
-        IValidator<DamageClaimUpdateDto> updateValidator)
+    public Task<List<DamageClaimResponseDto>> GetAllDamageClaimsAsync()
     {
-        public Task<List<DamageClaimResponseDto>> GetAllDamageClaimsAsync()
-        {
-            var damageClaims = damageClaimRepository.GetAll().ToList();
-            var response = mapper.Map<List<DamageClaimResponseDto>>(damageClaims);
+        var damageClaims = damageClaimRepository.GetAll().ToList();
+        var response = mapper.Map<List<DamageClaimResponseDto>>(damageClaims);
 
-            return Task.FromResult(response);
-        }
+        return Task.FromResult(response);
+    }
 
-        public async Task<DamageClaimResponseDto?> GetDamageClaimByIdAsync(Guid id)
-        {
-            var damageClaim = await damageClaimRepository.GetByIdAsync(id);
-            return damageClaim == null ? null : mapper.Map<DamageClaimResponseDto>(damageClaim);
-        }
+    public async Task<DamageClaimResponseDto?> GetDamageClaimByIdAsync(Guid id)
+    {
+        var damageClaim = await damageClaimRepository.GetByIdAsync(id);
+        return damageClaim == null ? null : mapper.Map<DamageClaimResponseDto>(damageClaim);
+    }
 
-        public async Task<DamageClaimResponseDto> CreateDamageClaimAsync(DamageClaimCreateDto dto)
-        {
-            var validation = await createValidator.ValidateAsync(dto);
-            if (!validation.IsValid)
-                throw new ValidationException(validation.Errors);
+    public async Task<DamageClaimResponseDto> CreateDamageClaimAsync(DamageClaimCreateDto dto)
+    {
+        var validation = await createValidator.ValidateAsync(dto);
+        if (!validation.IsValid)
+            throw new ValidationException(validation.Errors);
 
-            var damageClaim = mapper.Map<DamageClaim>(dto);
-            await damageClaimRepository.AddAsync(damageClaim);
-            return mapper.Map<DamageClaimResponseDto>(damageClaim);
-        }
+        var damageClaim = mapper.Map<DamageClaim>(dto);
+        await damageClaimRepository.AddAsync(damageClaim);
+        return mapper.Map<DamageClaimResponseDto>(damageClaim);
+    }
 
-        public async Task<bool> UpdateDamageClaimAsync(DamageClaimUpdateDto dto)
-        {
-            var validation = await updateValidator.ValidateAsync(dto);
-            if (!validation.IsValid)
-                throw new ValidationException(validation.Errors);
+    public async Task<bool> UpdateDamageClaimAsync(DamageClaimUpdateDto dto)
+    {
+        var validation = await updateValidator.ValidateAsync(dto);
+        if (!validation.IsValid)
+            throw new ValidationException(validation.Errors);
 
-            var damageClaim = await damageClaimRepository.GetByIdAsync(dto.Id);
-            if (damageClaim == null) return false;
+        var damageClaim = await damageClaimRepository.GetByIdAsync(dto.Id);
+        if (damageClaim == null) return false;
 
-            mapper.Map(dto, damageClaim);
-            await damageClaimRepository.UpdateAsync(damageClaim);
-            return true;
-        }
+        mapper.Map(dto, damageClaim);
+        await damageClaimRepository.UpdateAsync(damageClaim);
+        return true;
+    }
 
-        public async Task<bool> DeleteDamageClaimAsync(Guid id)
-        {
-            var damageClaim = await damageClaimRepository.GetByIdAsync(id);
-            if (damageClaim == null) return false;
+    public async Task<bool> DeleteDamageClaimAsync(Guid id)
+    {
+        var damageClaim = await damageClaimRepository.GetByIdAsync(id);
+        if (damageClaim == null) return false;
 
-            await damageClaimRepository.DeleteAsync(id);
-            return true;
-        }
+        await damageClaimRepository.DeleteAsync(id);
+        return true;
     }
 }
--- TaskManagement.Application/Services/InsurancePolicyService.cs
+++ TaskManagement.Application/Services/InsurancePolicyService.cs
@@ -5,58 +5,57 @@ using TaskManagement.Application.Interfaces.Repositories;
 using TaskManagement.Domain.Office.Relation.InsurancePolicy;
 using TaskManagement.DTO.Office.Relation.InsurancePolicy;
 
-namespace TaskManagement.Application.Services
+namespace TaskManagement.Application.Services;
+
+public class InsurancePolicyService(
+    IInsurancePolicyRepository insurancePolicyRepository,
+    IMapper mapper,
+    IValidator<InsurancePolicyCreateDto> createValidator,
+    IValidator<InsurancePolicyUpdateDto> updateValidator)
 {
-    public class InsurancePolicyService(
-        IInsurancePolicyRepository insurancePolicyRepository,
-        IMapper mapper,
-        IValidator<InsurancePolicyCreateDto> createValidator,
-        IValidator<InsurancePolicyUpdateDto> updateValidator)
+    public async Task<List<InsurancePolicyResponseDto>> GetAllInsurancePoliciesAsync()
     {
-        public async Task<List<InsurancePolicyResponseDto>> GetAllInsurancePoliciesAsync()
-        {
-            var insurancePolicies = await insurancePolicyRepository.GetAll().ToListAsync();
-            return mapper.Map<List<InsurancePolicyResponseDto>>(insurancePolicies);
-        }
+        var insurancePolicies = await insurancePolicyRepository.GetAll().ToListAsync();
+        return mapper.Map<List<InsurancePolicyResponseDto>>(insurancePolicies);
+    }
 
-        public async Task<InsurancePolicyResponseDto?> GetInsurancePolicyByIdAsync(Guid id)
-        {
-            var insurancePolicy = await insurancePolicyRepository.GetByIdAsync(id);
-            return insurancePolicy == null ? null : mapper.Map<InsurancePolicyResponseDto>(insurancePolicy);
-        }
+    public async Task<InsurancePolicyResponseDto?> GetInsurancePolicyByIdAsync(Guid id)
+    {
+        var insurancePolicy = await insurancePolicyRepository.GetByIdAsync(id);
+        return insurancePolicy == null ? null : mapper.Map<InsurancePolicyResponseDto>(insurancePolicy);
+    }
 
-        public async Task<InsurancePolicyResponseDto> CreateInsurancePolicyAsync(InsurancePolicyCreateDto dto)
-        {
-            var validation = await createValidator.ValidateAsync(dto);
-            if (!validation.IsValid)
-                throw new ValidationException(validation.Errors);
+    public async Task<InsurancePolicyResponseDto> CreateInsurancePolicyAsync(InsurancePolicyCreateDto dto)
+    {
+        var validation = await createValidator.ValidateAsync(dto);
+        if (!validation.IsValid)
+            throw new ValidationException(validation.Errors);
 
-            var insurancePolicy = mapper.Map<InsurancePolicy>(dto);
-            await insurancePolicyRepository.AddAsync(insurancePolicy);
-            return mapper.Map<InsurancePolicyResponseDto>(insurancePolicy);
-        }
+        var insurancePolicy = mapper.Map<InsurancePolicy>(dto);
+        await insurancePolicyRepository.AddAsync(insurancePolicy);
+        return mapper.Map<InsurancePolicyResponseDto>(insurancePolicy);
+    }
 
-        public async Task<bool> UpdateInsurancePolicyAsync(InsurancePolicyUpdateDto dto)
-        {
-            var validation = await updateValidator.ValidateAsync(dto);
-            if (!validation.IsValid)
-                throw new ValidationException(validation.Errors);
+    public async Task<bool> UpdateInsurancePolicyAsync(InsurancePolicyUpdateDto dto)
+    {
+        var validation = await updateValidator.ValidateAsync(dto);
+        if (!validation.IsValid)
+            throw new ValidationException(validation.Errors);
 
-            var insurancePolicy = await insurancePolicyRepository.GetByIdAsync(dto.Id);
-            if (insurancePolicy == null) return false;
+        var insurancePolicy = await insurancePolicyRepository.GetByIdAsync(dto.Id);
+        if (insurancePolicy == null) return false;
 
-            mapper.Map(dto, insurancePolicy);
-            await insurancePolicyRepository.UpdateAsync(insurancePolicy);
-            return true;
-        }
+        mapper.Map(dto, insurancePolicy);
+        await insurancePolicyRepository.UpdateAsync(insurancePolicy);
+        return true;
+    }
 
-        public async Task<bool> DeleteInsurancePolicyAsync(Guid id)
-        {
-            var insurancePolicy = await insurancePolicyRepository.GetByIdAsync(id);
-            if (insurancePolicy == null) return false;
+    public async Task<bool> DeleteInsurancePolicyAsync(Guid id)
+    {
+        var insurancePolicy = await insurancePolicyRepository.GetByIdAsync(id);
+        if (insurancePolicy == null) return false;
 
-            await insurancePolicyRepository.DeleteAsync(id);
-            return true;
-        }
+        await insurancePolicyRepository.DeleteAsync(id);
+        return true;
     }
 }
