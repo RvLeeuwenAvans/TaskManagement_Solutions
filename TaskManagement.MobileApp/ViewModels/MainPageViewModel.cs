91f78bca250b8de576ce422e973de57013563252
Ridwan <r.vanleeuwen2@student.avans.nl>
2025-05-07 09:48:14 +0200

    Add mainpage overview; add Maui community toolkit and started working on the taskCards

---
+++ TaskManagement.MobileApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace TaskManagement.MobileApp.ViewModels;
+
+public class MainPageViewModel
+{
+    public ObservableCollection<TaskItem> TaskItems { get; set; }
+
+    public MainViewModel()
+    {
+        TaskItems = new ObservableCollection<TaskItem>
+        {
+            new TaskItem { IconLetter = "H", Title = "Foto toevoegen", Subtitle = "Relatie: Henk van den Rooijboom", DateText = "16-7-2023" },
+            new TaskItem { IconLetter = "K", Title = "Rapport uploaden", Subtitle = "Relatie: Kim Vermeer", DateText = "17-7-2023" },
+            // Add more tasks
+        };
+    }
+}
---
+++ TaskManagement.MobileApp/Views/Pages/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace TaskManagement.MobileApp.Views.Pages;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    
+    /**
+     * The tabs on the main page ore not actually tabs but filters; but to mock tab behavior, we show/hide the underline.
+     */
+    private void OnTabClicked(object sender, EventArgs e)
+    {
+        AllUnderline.IsVisible = false;
+        TodayUnderline.IsVisible = false;
+        WeekUnderline.IsVisible = false;
+
+        if (sender == AllButton)
+            AllUnderline.IsVisible = true;
+        else if (sender == TodayButton)
+            TodayUnderline.IsVisible = true;
+        else if (sender == WeekButton)
+            WeekUnderline.IsVisible = true;
+    }
+}
