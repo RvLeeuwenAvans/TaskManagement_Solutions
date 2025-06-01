607712896d4d5ca8cfcaee802f8d2bd4f496b9d9
Ridwan <r.vanleeuwen2@student.avans.nl>
2025-06-01 11:35:18 +0200

    renamed styles; did some refactoring for readability/ consistency. Also added some fixes and workarounds to make the winui application look more like the mockups/android verion of the application.

--- src/Mobile/TaskManagement.MobileApp/src/Views/TaskFormView.xaml.cs
+++ src/Mobile/TaskManagement.MobileApp/src/Views/TaskFormView.xaml.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TaskManagement.MobileApp.Views;
+﻿namespace TaskManagement.MobileApp.Views;
 
 public partial class TaskFormView : ContentView
 {
     public TaskFormView()
     {
         InitializeComponent();
+    }
+
+    private void OnContainerSizeChanged(object sender, EventArgs e)
+    {
+        // this code only exists because winui3 is dumb and uses other sizing logic for the datepicker element.
+        // if they ever make this properly work with maui; delete this please.
+        if (sender is Grid container && DeadlinePicker != null)
+        {
+            #if WINDOWS
+                DeadlinePicker.WidthRequest = container.Width;
+            #endif
+        }
     }
 }
