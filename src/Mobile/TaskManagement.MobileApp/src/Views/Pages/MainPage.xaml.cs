67feaaf9655a75a2d1ffe3a315634c32128ea5ad
Ridwan <r.vanleeuwen2@student.avans.nl>
2025-06-10 14:05:34 +0200

    removed some resolved todo items

--- src/Mobile/TaskManagement.MobileApp/src/Views/Pages/MainPage.xaml.cs
+++ src/Mobile/TaskManagement.MobileApp/src/Views/Pages/MainPage.xaml.cs
@@ -3,10 +3,6 @@ using TaskManagement.MobileApp.ViewModels;
 
 namespace TaskManagement.MobileApp.Views.Pages;
 
-/**
- * todo Look into: https://learn.microsoft.com/en-us/dotnet/architecture/maui/mvvm
- * for commands; seems more idiomatic.. than the onclicks here.
- */
 public partial class MainPage : ContentPage
 {
     public MainPage(TaskService taskService, OfficeService officeService, LinkedObjectService linkedObjectService)
