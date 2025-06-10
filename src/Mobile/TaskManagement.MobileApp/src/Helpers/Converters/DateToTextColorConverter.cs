cf18f2517c686ea899dd86ecd17265ae20c5f323
Ridwan <r.vanleeuwen2@student.avans.nl>
2025-06-10 14:01:49 +0200

    Added converters for dynamic DueDate color changes and updated styles for TaskCardView. To show the user more easily if i task is in the past present or future.

---
+++ src/Mobile/TaskManagement.MobileApp/src/Helpers/Converters/DateToTextColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace TaskManagement.MobileApp.Helpers.Converters;
+
+public class DateToTextColorConverter : IValueConverter
+{
+    public required Color PastDueColor { get; set; }
+    public required Color TodayColor { get; set; }
+    public required Color FutureColor { get; set; }
+    
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not DateTime dueDate) return FutureColor;
+        
+        var today = DateTime.Today;
+        var dueDateOnly = dueDate.Date;
+
+        return (dueDateOnly.CompareTo(today)) switch
+        {
+            < 0 => PastDueColor,
+            0 => TodayColor,
+            _ => FutureColor
+        };
+    }
+
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
