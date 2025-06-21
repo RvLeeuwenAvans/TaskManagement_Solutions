c4dc29aef5d985f11b2b6d2e4b6df7cf577455d7
Ridwan <r.vanleeuwen2@student.avans.nl>
2025-06-21 21:59:46 +0200

    last couple of fixes

--- src/Mobile/TaskManagement.MobileApp/src/ViewModels/Modals/TaskTypeFilterModalViewModel.cs
+++ src/Mobile/TaskManagement.MobileApp/src/ViewModels/Modals/TaskTypeFilterModalViewModel.cs
@@ -1,12 +1,10 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using TaskManagement.MobileApp.ViewModels.messages;
 
 namespace TaskManagement.MobileApp.ViewModels.Modals;
 
-[SuppressMessage("CommunityToolkit.Mvvm.SourceGenerators.ObservablePropertyGenerator", "MVVMTK0045:Using [ObservableProperty] on fields is not AOT compatible for WinRT")]
 public partial class TaskTypeFilterModalViewModel(TaskTypeFilter currentFilter) : ObservableObject
 {
     [ObservableProperty] private TaskTypeFilter _selectedType = currentFilter;
